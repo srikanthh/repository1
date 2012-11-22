dca1d4a3ff67a31eddee51e535feb2c2cb785b80
srikanthh <ksrikanth1990@gmail.com>
2012-11-22 13:57:05 +0530

    Update sample1/src/com/welcome/Sample1Servlet.java

--- sample1/src/com/welcome/Sample1Servlet.java
+++ sample1/src/com/welcome/Sample1Servlet.java
@@ -8,5 +8,6 @@ public class Sample1Servlet extends HttpServlet {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
-	//test by srk
+	//test by srk1
+	//test by srk2
 }
